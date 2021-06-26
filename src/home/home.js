163689222976f03d6e5d9ce21dd981034daff6fb
Pham Tinh <pr.phamtinh@gmail.com>
2021-06-26 16:55:24 +0700

    css

--- src/home/home.js
+++ src/home/home.js
@@ -1,52 +1,94 @@
 import React, { useState } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addProduct, removeProduct } from './actions';
+import './css.css';
 
 function Home(props) {
-    const dispatch = useDispatch();
-    const state = useSelector(state => state);
-    const [product, setProduct] = useState({});
-    const [id, setId] = useState('');
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
+  const [product, setProduct] = useState({});
 
-    console.log(props)
+  console.log(props)
 
-    const onDispatchAddProduct = () => {
-        console.log(product)
-        if (!product.name) return;
-        dispatch(addProduct(product))
-    }
+  const onDispatchAddProduct = () => {
+    console.log(product)
+    if (!product.name) return;
+    dispatch(addProduct(product))
+  }
 
-    const handleChangeProduct = (inputName, e) => {
-        setProduct(prev => ({ ...prev, [inputName]: e.target.value }))
-    }
+  const handleChangeProduct = (inputName, e) => {
+    setProduct(prev => ({ ...prev, [inputName]: e.target.value }))
+  }
 
-    console.log('useSelecter', state.home.products)
+  const handleDel = (product) => {
+    dispatch(removeProduct(product.id))
+  }
 
-    return <>
-        <h1>hahhahaa</h1>
-        <input type="text" onChange={(e) => handleChangeProduct('id', e)} />
-        <input type="text" onChange={(e) => handleChangeProduct('name', e)} />
-        <input type="text" onChange={(e) => handleChangeProduct('amount', e)} />
+  console.log('useSelecter', state.home.products)
 
-        <button onClick={onDispatchAddProduct}> add number</button>
+  return <>
+    <h1>Test redux</h1>
+    <div>
+      <span>id: </span>
+      <input style={{ border: '1px solid #d3d3d3', padding: 8, borderRadius: 4 }} type="text" onChange={(e) => handleChangeProduct('id', e)} />
+    </div>
+    <div>
+      <span>Mầy gì thế: </span>
+      <input
+        style={{ border: '1px solid #d3d3d3', padding: 8, borderRadius: 4 }} type="text"
+        onChange={(e) => handleChangeProduct('name', e)}
+      />
+    </div>
 
+    <div>
+      <span>Mấy tuổi rồi: </span>
+      <input
+        style={{ border: '1px solid #d3d3d3', padding: 8, borderRadius: 4 }} type="number"
+        onChange={(e) => handleChangeProduct('amount', e)}
+      />
+    </div>
+    <div>
+      <button
+        onClick={onDispatchAddProduct}
+        style={{ background: 'blue', color: 'white', border: 'none', padding: 8, borderRadius: 4 }}
+      >Thêm</button>
+    </div>
 
+    <table>
+      <thead>
+        <tr style={{ background: '#006FCA' }}>
+          <th className="th-table"  >STT</th>
+          <th className="th-table"  >ID</th>
+          <th className="th-table"  >Họ và tên</th>
+          <th className="th-table" >Đơn giá/Ngày</th>
+          <th className="th-table" >Thao tác</th>
+        </tr>
+      </thead>
+
+      <tbody>
         {
-            state.home.products ?
-                state.home.products.map((product, i) => <div key={product.id}>
-                    <div style={{ display: 'flex', margin: 8, padding: 8, background: 'red', color: '#fff' }}>
-                        <div>{i + 1}</div>
-                        <div>{product.id}</div> -
-                        <div>{product.name}</div> -
-                        <div>{product.amount}</div>
-                    </div>
-                </div>)
-                : <>No data </>
+          state.home.products ?
+            state.home.products.map((product, i) =>
+              <tr key={product.id}>
+                <td>{i + 1}</td>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>{product.amount}</td>
+                <td>
+                  <button
+                    onClick={() => handleDel(product)}
+                    style={{ background: 'red', color: 'white', border: 'none', padding: 8, borderRadius: 4 }}
+                  >Xóa</button></td>
+              </tr>)
+            : <tr> <td colSpan={5}>No data</td></tr>
         }
+      </tbody>
+    </table>
 
-        <input type="text" onChange={(e) => setId(e.target.value)} />
-        <button onClick={() => dispatch(removeProduct(id))}>remove number</button>
-    </>
+    {/* 
+    <input type="text" onChange={(e) => setId(e.target.value)} />
+    <button onClick={() => dispatch(removeProduct(id))}>remove number</button> */}
+  </>
 }
 
 export default Home
